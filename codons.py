c0bd79b40d4169c2d5ae6ccc2ad17ca358b05251
tshavit2011-bit <tshavit2011@gmail.com>
2025-12-09 20:54:54 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,4 +1,13 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  dict = {}
+  file = open(file_path)
+  rows = file.readlines()
+  file.close()
+  for i in range(len(rows)):
+    row = rows[i]
+    codon = row.strip().split('\t')[0]
+    acid = row.strip().split('\t')[2]
+    dict[codon] = acid
+  return dict
 
 
